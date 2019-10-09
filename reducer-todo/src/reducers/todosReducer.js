889766e317169cff76543c304cdcab2a840a9ad4
David Kuseh <davegru203@gmail.com>
2019-10-09 02:03:30 +0100

    refactored reducer function into separate file

--- reducer-todo/src/reducers/todosReducer.js
+++ reducer-todo/src/reducers/todosReducer.js
@@ -1,15 +1,47 @@
-import React from 'react';
-
-export const todosList = [
-    {item: 'Learn about reducers', completed: false, id: 1},
-    {item: 'Read on ContextAPI', completed: false, id: 2},
-    {item: 'Revise destructuring', completed: false, id: 3},
-]
-
-
-export default function reducer(todos, action) {
-    switch (action.type) {
-        default:
-            return todos;
+export const initialTodoState = [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 3892987589
     }
-}
+  ];
+  export const initialState = {
+    todos: initialTodoState,
+    addTodo: ""
+  };
+  
+  export const ON_INPUT_CHANGE = "ON_INPUT_CHANGE";
+  export const ON_SUBMIT = "ON_SUBMIT";
+  export const ON_COMPLETE = "ON_COMPLETE";
+  export const ON_CLEAR_COMPLETED = "ON_CLEAR_COMPLETED";
+  
+  export function reducer(state, action) {
+    switch (action.type) {
+      case ON_INPUT_CHANGE:
+        return { ...state, ...action.payload };
+      case ON_SUBMIT:
+        return {
+          ...state,
+          todos: state.todos.concat({
+            item: state.addTodo,
+            completed: false,
+            id: Date.now()
+          }),
+          addTodo: ""
+        };
+      case ON_COMPLETE:
+        return { ...state, todos: state.todos.map(todo => {
+          if (action.payload.id !== todo.id) {
+            return todo;
+          } return {...todo, completed: action.payload.complete}
+        })};
+      case ON_CLEAR_COMPLETED:
+        let clearTodos = state.todos.filter(item => !item.completed)
+        return {
+            ...state,
+            todos: [...clearTodos]
+        }
+      default:
+        return state;
+    }
+  }
--- reducer-todo/src/todos.js
+++ reducer-todo/src/todos.js
@@ -1,52 +1,5 @@
 import React, { useReducer } from "react";
-
-const initialTodoState = [
-  {
-    item: "Learn about reducers",
-    completed: false,
-    id: 3892987589
-  }
-];
-const initialState = {
-  todos: initialTodoState,
-  addTodo: ""
-};
-
-const ON_INPUT_CHANGE = "ON_INPUT_CHANGE";
-const ON_SUBMIT = "ON_SUBMIT";
-const ON_COMPLETE = "ON_COMPLETE";
-const ON_CLEAR_COMPLETED = "ON_CLEAR_COMPLETED";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case ON_INPUT_CHANGE:
-      return { ...state, ...action.payload };
-    case ON_SUBMIT:
-      return {
-        ...state,
-        todos: state.todos.concat({
-          item: state.addTodo,
-          completed: false,
-          id: Date.now()
-        }),
-        addItem: ""
-      };
-    case ON_COMPLETE:
-      return { ...state, todos: state.todos.map(todo => {
-        if (action.payload.id !== todo.id) {
-          return todo;
-        } return {...todo, completed: action.payload.complete}
-      })};
-    case ON_CLEAR_COMPLETED:
-      let clearTodos = state.todos.filter(item => !item.completed)
-      return {
-          ...state,
-          todos: [...clearTodos]
-      }
-    default:
-      return state;
-  }
-}
+import {initialState, reducer, ON_CLEAR_COMPLETED, ON_COMPLETE, ON_INPUT_CHANGE, ON_SUBMIT} from './reducers/todosReducer';
 
 export default function Todos() {
   const [state, dispatch] = useReducer(reducer, initialState);
